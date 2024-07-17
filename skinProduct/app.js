f9104e2ccf534820f126f09546e98cd5ef58de51
MasiSerhan <maisserhan09@gmail.com>
2024-07-17 11:58:30 +0300

    first commit

--- skinProduct/app.js
+++ skinProduct/app.js
@@ -6,6 +6,7 @@ let body = document.querySelector('body');
 let closeCart = document.querySelector('.close');
 const nav=document.querySelector(".navbar__menu");
 let ul=document.getElementById("navbar__list");
+let asul=document.getElementById("aside__list");
 let products = [];
 let cart = [];
 
@@ -15,16 +16,22 @@ const navbar= ()=>{
     close.innerHTML="X";
     close.setAttribute("onclick",`show("#navbar__list")`);
     ul.appendChild(close);
+    
     close.classList.add("off");
     
-    for(let i=1;i<5;i++){
+    for(let i=0;i<5;i++){
     let n=`Section ${i}`;
     let c=`section${i}`;
     let a =`<a class="menu__link" id="${c}" href="#${c}" ></a>`;
     let li=document.createElement("li");
     li.setAttribute('class',`nav__item ${c}`);
+    let li2=document.createElement("li");
+    li2.setAttribute('class',`nav__item ${c}`);
     ul.appendChild(li);
+    asul.appendChild(li2);
     let s = document.querySelector(`.${c}`);
+    s.innerHTML=a;
+    s = document.querySelector(`aside .${c}`);
     s.innerHTML=a;
     };
 
@@ -37,6 +44,18 @@ const navbar= ()=>{
     a.innerHTML = "تعرف علينا ";  
     a =document.querySelector("#section1");
     a.innerHTML = "العروض "; 
+
+    a =document.querySelector("#aside__list #section4");
+    a.innerHTML = "الرئيسية";
+    a.setAttribute("href","//gigi-boutque.vercel.app/")
+    a =document.querySelector("#aside__list #section3");
+    a.innerHTML = `<button class="on" onclick="show1('#aside__list2')">منتجاتنا </button>`;  
+    a =document.querySelector("#aside__list #section2");
+    a.innerHTML = "تعرف علينا ";  
+    a =document.querySelector("#aside__list #section1");
+    a.innerHTML = "العروض "; 
+    a =document.querySelector("#aside__list #section0");
+    a.innerHTML = "التواصل والاستفسار "; 
     
   };
   
@@ -44,52 +63,50 @@ const navbar= ()=>{
 
 
 const minue=()=>{
+  let nav1=document.querySelector(".nav1");
   let button =document.createElement("button");
   button.setAttribute("class","menu__link");
   button.setAttribute("id","minue");
   button.setAttribute("onclick", `show("#navbar__list")`);
   button.innerHTML=`<img src="https://static.vecteezy.com/system/resources/previews/002/292/406/non_2x/hamburger-menu-line-icon-free-vector.jpg " width="40px" height="40px">`
-  nav.insertAdjacentElement("afterbegin",button);
+  nav1.insertAdjacentElement("afterbegin",button);
 }
 
 minue();
 
-
+let d=0;
 function show(str){
-    let ul3=document.getElementById("navbar__list2");
+    asul=document.querySelector("aside");
     let mnue=document.querySelector(str);
     let display=mnue.style.display;
-    if(display=="none"){
-    mnue.style.display= "flex";
+    if(d==0){
+        asul.classList.add("aside");
+        d=1;   
     }
-
-    else if(display=="flex"){
-      mnue.style.display= "none";
-      ul3.style.display="none";
-    }
-
     else{
-        mnue.style.display="flex";  
+        asul.classList.remove("aside"); 
+        d=0;
     }
   }
 
 function show1(str){
     let mnue=document.querySelector(str);
+    let cont=document.querySelector(".container ");
     let display=mnue.style.display;
     if(display=="none"){
     mnue.style.display= "flex";
+    cont.classList.remove("hi");
     }
     
-    else if(display=="flex"){
-        mnue.style.display= "none";
-    }
     else{
-        mnue.style.display="flex";  
+        mnue.style.display= "none";
+        cont.classList.add("hi");
     }
  
   }
 
   ul=document.getElementById("navbar__list2");
+  asul=document.getElementById("aside__list2");
   const product= ()=>{
       for(let i=0;i<9;i++){
       let n=`Section ${i+5}`;
@@ -97,11 +114,37 @@ function show1(str){
       let a =`<a class="menu__link" id="${c}" href="#${c}" ></a>`;
       let li=document.createElement("li");
       li.setAttribute('class',`nav__item ${c}`);
+      let li2=document.createElement("li");
+      li2.setAttribute('class',`nav__item ${c}`);
       ul.appendChild(li);
+      asul.appendChild(li2);
       let s = document.querySelector(`.${c}`);
       s.innerHTML=a;
+      s = document.querySelector(`aside .${c}`);
+      s.innerHTML=a;
       };
-      let a =document.querySelector("#section13");
+      let a =document.querySelector("#aside__list2 #section13");
+      a.innerHTML = "اكسسوارات";
+      a.setAttribute("href","//gigi-boutque.vercel.app/")
+      a =document.querySelector("#aside__list2 #section12");
+      a.innerHTML = "عطور رجالية ";    
+      a =document.querySelector("#aside__list2 #section11");
+      a.innerHTML = " عطور نسائية ";  
+      a =document.querySelector("#aside__list2 #section10");
+      a.innerHTML = "منتجات الحامل "; 
+      a =document.querySelector("#aside__list2 #section9");
+      a.innerHTML = "منتجات الشعر "; 
+      a =document.querySelector("#aside__list2 #section8");
+      a.innerHTML = "منتجات القدم واليدين "; 
+      a =document.querySelector("#aside__list2 #section7");
+      a.innerHTML = "منتجات الجسم "; 
+      a =document.querySelector("#aside__list2 #section6");
+      a.innerHTML = "منتجات البشرة ";
+      a =document.querySelector("#aside__list2 #section5");
+      a.innerHTML = " make up ";
+      a.style.color="#b69292";
+
+      a =document.querySelector("#section13");
       a.innerHTML = "اكسسوارات";
       a.setAttribute("href","//gigi-boutque.vercel.app/")
       a =document.querySelector("#section12");
@@ -120,7 +163,12 @@ function show1(str){
       a.innerHTML = "منتجات البشرة ";
       a =document.querySelector("#section5");
       a.innerHTML = " make up ";
-      a.style.color="#b69292"
+      a.style.color="#b69292";
+      let close2=document.createElement("button");
+      close2.setAttribute("onclick",`show("#navbar__list")`);
+      close2.innerHTML="X";
+      asul=document.querySelector("aside");
+      asul.appendChild(close2);
 };
   
 product();
@@ -267,8 +315,3 @@ const initApp = () => {
 initApp();
 
 
-
-
-
-let s=document.querySelector(`[class="item]`);
-console.log(s);
